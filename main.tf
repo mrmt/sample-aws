086abb74168fd832a476ec1c234b9aa8df063f8c
jun.morimoto.a <morimoto@gmail.com>
2024-08-29 15:23:52 +0900

    update

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "aws_instance" "app_server" {
   instance_type = "t2.micro"
 
   tags = {
-    Name = "Handson_Jun_Morimoto"
+    Name = "Handson_JunMorimoto_2024-08-29T1522"
     TED = "20240829_Handson"
   }
 }
